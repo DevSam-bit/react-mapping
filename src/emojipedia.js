81c1027da0da595b4f54be4d3c74ca9291ca5c30
DevSam-bit <78569629+DevSam-bit@users.noreply.github.com>
2021-06-14 01:57:37 +0300

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🍔",
+    name: "Hamburger",
+    meaning:
+      "I love American food! The fast food classic par excellence: sesame rolls topped with a kind of ground beef steak, tomato, onion slices and lettuce. Used in connection with the United States or food in general."
+  },
+  {
+    id: 5,
+    emoji: "😍",
+    name: "Smiling face with heart-eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  },
+  {
+    id: 6,
+    emoji: "💗",
+    name: "Red Heart",
+    meaning:
+      "The red heart is the classical symbol of love. Expression of passion and romance. But also in the non-romantic context for friendship and strong bonds."
+  }
+];
+
+export default emojipedia;
